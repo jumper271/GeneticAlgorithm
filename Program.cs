21ee6a0eaac57cc008fae3e4d9c45bc00267e829
jumper271 <hanna-alberti@t-online.de>
2025-02-22 22:01:22 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_Hausarbeit
+{
+    internal static class Program
+    {
+        //[DllImport("kernel32.dll")]
+        //static extern bool AllocConsole();
+
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //AllocConsole(); // Konsole starten
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
